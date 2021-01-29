ba5e90077fd14425f15fd6957f53d44efa2b19d0
VitoJeong <ckdghk987@gmail.com>
2021-01-29 23:08:46 +0900

    DataStructure_List_04

---
+++ DataStructure/src/list/arraylist/implementation/Instance.java
@@ -0,0 +1,14 @@
+package list.arraylist.implementation;
+
+import java.util.ArrayList;
+
+public class Instance {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> numbers = new ArrayList<>();
+		
+
+	}
+
+}
