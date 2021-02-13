8bc9d86468a95056ed1a16ead7389631c9d75986
VitoJeong <ckdghk987@gmail.com>
2021-02-13 23:41:48 +0900

    로또머신_중복해결

--- ToyPjt/src/LottoMachine.java
+++ ToyPjt/src/LottoMachine.java
@@ -9,28 +9,7 @@ public class LottoMachine {
 	
 	public static void main(String[]args) throws IOException{
 		
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		
-//		System.out.println("========최근 5회의 당첨 번호를 입력해주세요=========");
-//		
-//		List<Numbers> list = new ArrayList<Numbers>();
-//
-//		for(int i=0;i<5;i++){
-//			
-//			Numbers numbers = new Numbers();
-//			System.out.println("지난"+ (i+1)+"회의 당첨 번호를 입력해주세요");
-//			numbers.setNum1(Byte.parseByte(br.readLine()));
-//			numbers.setNum2(Byte.parseByte(br.readLine()));
-//			numbers.setNum3(Byte.parseByte(br.readLine()));
-//			numbers.setNum4(Byte.parseByte(br.readLine()));
-//			numbers.setNum5(Byte.parseByte(br.readLine()));
-//			numbers.setNum6(Byte.parseByte(br.readLine()));
-//			System.out.print("보너스를 입력해주세요! >>>");
-//			numbers.setBonus(Byte.parseByte(br.readLine()));
-//			
-//			list.add(numbers);
-//			System.out.println("최근 "+ i+"회의 당첨번호가 저장되었습니다.");
-//		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		List<Byte> result = new ArrayList<>();
 		
@@ -39,41 +18,31 @@ public class LottoMachine {
 			numbers[i] = (byte) (i+1);
 		}
 		
-//		for(byte i=0;i<numbers.length;i++){
-//			System.out.print(numbers[i]+" ");
-//		}
-		
-		for(byte i=0;i<7;i++){
+		while(result.size()<7){
 			byte ball = (byte) (Math.random() * 45 + 1);
-			if(checkDuplicated(result, ball)!=0) result.add(ball);
+			if(!result.contains(ball)) {result.add(ball);}
+			else {
+				System.out.println("중복>,.<");
+			}
 		}
+		
+		result.sort(null);
 		System.out.println("로또번호 추첨 고고링 !!");
 		for(byte i=0;i<result.size();i++){
 			try {
-				Thread.sleep(1);
+				Thread.sleep(1000);
 				if(!(i==result.size()-1)){
 					
-					System.out.println(i+1 + "번 번호" + result.get(i));
+					System.out.println(i+1 + "번 번호 : " + result.get(i));
 				}
 				else {
 					
-					System.out.println("보너스 번호" + result.get(i));
+					System.out.println("보너스! :" + result.get(i));
 				}
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-	}
+	} // main() 
 	
-	public static byte checkDuplicated(List<Byte> list, byte n){
-		byte result = 0;
-		if(list.contains(n)) {
-			byte ball = (byte) (Math.random() * 45 + 1);
-			result = checkDuplicated(list, ball);
-		}
-		else{
-			result = n;
-		}
-		return result;
-	}
 }
