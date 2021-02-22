fe92ac13ec604b344e8b4496fcd9ea756cbcb25a
VitoJeong <ckdghk987@gmail.com>
2021-02-22 22:24:17 +0900

    Baekjoon_1181/1431

--- CodingTest/src/Baekjoon.java
+++ CodingTest/src/Baekjoon.java
@@ -1,33 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Baekjoon {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		// 1181 - 단어정렬
+		// 10989 - 수 정렬하기3
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int a = Integer.parseInt(br.readLine());
-		String[] arr = new String[a];
+		short[] cnt = new short[10001];
 		
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = br.readLine();
+		for (int i = 0; i < a; i++) {
+			short num = Short.parseShort(br.readLine());
+			cnt[num]++;
 		}
 		
-		arr = sort(arr);
+		
+		for (int i = 0; i < cnt.length; i++) {
 
+			while(cnt[i]!=0){
+				System.out.println(i);
+				cnt[i]--;
+			}
+		}
 		
 	}
 
-	private static String[] sort(String[] arr) {
-		
-		for (int i = 0; i < arr.length; i++) {
-			
-		}
-		
-		return null;
-	}
-	
 
 }
