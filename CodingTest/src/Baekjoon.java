2bed7f3fc476967a0d9acdb7978903bd67905f55
VitoJeong <ckdghk987@gmail.com>
2021-03-19 22:43:29 +0900

    1516 게임개발 _1

--- CodingTest/src/Baekjoon.java
+++ CodingTest/src/Baekjoon.java
@@ -11,33 +11,45 @@ import java.util.StringTokenizer;
 public class Baekjoon {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		// 2252 줄세우기
-		// -> 위상정렬 사용
+		// 1516 게임개발
+		// -> 위상정렬 사용(임계변수 사용)
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
 		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
 	
-		// 학생수의 차수
+		// 차수
 		int inDegree[] = new int[n];
 		ArrayList<Integer> adj[] = new ArrayList[n];
+		int result[] = new int[n]; // 최소시간을 저장
+		int total[] = new int[n];
 		
 		for (int i = 0; i < inDegree.length; i++) {
 			adj[i] = new ArrayList<Integer>();
 		}
 		
-		for (int i = 0; i < m; i++) {
+		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			int y = Integer.parseInt(st.nextToken())-1;
-			int x = Integer.parseInt(st.nextToken())-1;
-			adj[y].add(x);
-			inDegree[x]++;
+//			System.out.println(st);
+			int x = Integer.parseInt(st.nextToken());
+			result[i]=x;
+			while(true){
+				x = Integer.parseInt(st.nextToken())-1;
+				System.out.println(x);
+				if(x==-2)break;
+				inDegree[i]++;
+				adj[x].add(i);
+			}
 		}
+		
+//		for (int i = 0; i < result.length; i++) {
+//			System.out.println(result[i]); 
+//		}
 		
 		LinkedList<Integer> q = new LinkedList<>();
 		for (int i = 0; i < adj.length; i++) {
 			if(inDegree[i] == 0){
+				System.out.println("dd");
 				q.offer(i);
 				inDegree[i]--;
 			}
@@ -45,9 +57,11 @@ public class Baekjoon {
 		
 		while(!q.isEmpty()){
 			int x = q.poll();
-			System.out.print((x+1)+" ");
 			for(int i=0;i<adj[x].size();i++){
 				int ne = adj[x].get(i);
+				System.out.println("ne : " + result[ne]);
+				result[ne] = Math.max(result[ne], result[x] + result[ne]); 
+				
 				inDegree[ne]--;
 				if(inDegree[ne]==0){
 					q.offer(ne);
@@ -55,6 +69,9 @@ public class Baekjoon {
 			}
 		}
 		
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]); 
+		}
 	}
 
 
