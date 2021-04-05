e2e8a9bc006348686e181784526ba9bd1eaa7035
VitoJeong <ckdghk987@gmail.com>
2021-04-05 23:56:38 +0900

    1671 Baekjoon

--- CodingTest/src/Baekjoon.java
+++ CodingTest/src/Baekjoon.java
@@ -11,9 +11,9 @@ import java.util.StringTokenizer;
 
 public class Baekjoon {
 
-	static int n, d[], stat[][];
+	static int n, d[];
 	static boolean[] v;
-	static List<Integer> arr[];
+	static Shark[] sharks;
 	
 	public static void main(String[] args) throws Exception {
 		// 1671
@@ -25,62 +25,85 @@ public class Baekjoon {
 		
 		n = Integer.parseInt(st.nextToken());
 
-		// 일에 처리할 수 있는 직원을 배정
 		d = new int[n+1];
-		stat = new int[n+1][3];
 		v = new boolean[n+1];
-		arr = new ArrayList[n+1];
+		sharks = new Shark[n+1];
 		
 		for (int i = 1; i <= n; i++) {
-			arr[i] = new ArrayList<Integer>();
-		}
-		for (int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < 3; j++) {
-				stat[i][j]= Integer.parseInt(st.nextToken());
-			}
+			int size = Integer.parseInt(st.nextToken());
+			int velocity = Integer.parseInt(st.nextToken());
+			int intelligence = Integer.parseInt(st.nextToken());
+			sharks[i] = new Shark(size, velocity, intelligence);
 		}
 
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++) {
-				if(stat[i][0] >= stat[j][0] && stat[i][1] >= stat[j][1] && stat[i][2] >= stat[j][2]){
-					arr[i].add(j);
-				}
-				else if(stat[i][0] <= stat[j][0] && stat[i][1] <= stat[j][1] && stat[i][2] <= stat[j][2]){
-					arr[j].add(i);
-				}
-			}
-		}
 		int result = 0;
 		for(int i = 1; i <= n; i++){
 			for (int j = 0; j < 2; j++) {
 				
 				v = new boolean[n+1];
+				System.out.println("ooo"+ i);
 				if(dfs(i)) result++;
 			}
 		}
-		System.out.println(result);
+		System.out.println(n-result);
+		for(int i = 1; i < d.length; i++){
+			System.out.println(d[i]);
+		}
 	}
 
 	
 	static boolean dfs(int x){
-		
-		for(int i = 0; i < arr[x].size(); i++){
-			
-			int w = arr[x].get(i);
 
-			if(!v[w])
-			{
-				v[w] = true;
+		for (int i = 1; i < sharks.length; i++) {
+			
+			if(i != x){
 				
-				if(d[w] == 0 || dfs(d[w])){
-					d[w] = x;
-					return true;
+				if(v[i]) continue;
+				
+				int compare = sharks[x].compareTo(sharks[i]);
+				
+				if((compare == 0 && x > i) || compare == 1){
+					v[i] = true;
+					if(d[i]==0 || dfs(d[i])){
+						System.out.printf("x : %d\n",x);
+						System.out.printf("i : %d\n",i);
+						System.out.println();
+						d[i] = x;
+						return true;
+					}
 				}
 			}
-			
 		}
 		return false;
 	}
 
+}
+
+class Shark implements Comparable<Shark>{
+	int size, velocity, intelligence;
+	
+	public Shark(int size, int velocity, int intelligence){
+		this.size = size;
+		this.velocity = velocity;
+		this.intelligence = intelligence;
+	}
+
+	@Override
+	public int compareTo(Shark shark) {
+		
+		if(this.size == shark.size && this.velocity == shark.velocity && this.intelligence == shark.intelligence)
+		{
+			return 0;
+		}
+		else if(this.size >= shark.size && this.velocity >= shark.velocity && this.intelligence >= shark.intelligence)
+		{
+			return 1;
+		}
+		else
+		{
+			return -1;
+		}
+	}
+	
 }
